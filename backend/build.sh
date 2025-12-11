34ffeb9c5bd80a4b7eaf79246abd4608b78b2551
Ester Alves <esteralves.s@icloud.com>
2025-12-11 12:50:14 +0000

    ajuste no script de build para migrações do banco de dados

--- backend/build.sh
+++ backend/build.sh
@@ -4,7 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate --fake-initial
+python manage.py migrate contenttypes 0001_initial --fake
+python manage.py migrate contenttypes
+python manage.py migrate
 
 python create_admin.py
